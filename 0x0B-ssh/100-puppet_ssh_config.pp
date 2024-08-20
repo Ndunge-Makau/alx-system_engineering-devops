4130763cb61525bd6b04b7fb893e5d3c9569268a
Ndunge-Makau <winniemakau@outloo.com>
2024-08-20 13:01:59 +0300

    Fix 100-puppet_ssh_config.pp: Set to edit /etc/ssh/ssh_config.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Set up your client SSH configuration file 
 
 file_line {'Turn off Password Authentication':
-  path => 'etc/ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no'
 }
 
 file_line {'Change Identify File':
-  path => 'etc/ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school'
 }
