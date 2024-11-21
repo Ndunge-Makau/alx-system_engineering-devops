0d3aadc74f882f938561959a7e5278d4b228598c
Ndunge-Makau <winniemakau@outloo.com>
2024-11-21 15:25:28 +0300

    Fix 2-puppet_custom_http_response_header.pp: Fix syntax.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,31 @@
 # Installs Nginx and connfigures server
 
-package {'nginx':
+exec { 'update':
+  command  => 'sudo apt-get update && sudo apt-get -y upgrade',
+  provider => shell,
+}
+
+package { 'nginx' :
   ensure    => installed,
 }
 
 file { '/var/www/html/index.nginx-debian.html' :
   ensure  => present,
   content => 'Hello World!',
+}
+
+file_line { 'redirect' :
+  ensure => present,
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;'
+  line  => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;"
+}
+
+file_line { '404' :
+  ensure => present,
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;'
+  line  => "\terror_page 404 /404_page.html;"
 }
 
 file_line { 'add header' :
