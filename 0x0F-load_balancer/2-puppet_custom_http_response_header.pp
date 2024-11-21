d73edc36c22a139ba78e13e126aa52381d087028
Ndunge-Makau <winniemakau@outloo.com>
2024-11-21 15:41:29 +0300

    Fix 2-puppet_custom_http_response_header.pp: Fix syntax.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,21 +17,21 @@ file { '/var/www/html/index.nginx-debian.html' :
 file_line { 'redirect' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;'
-  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;"
+  after  => 'server_name _;',
+  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;",
 }
 
 file_line { '404' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;'
-  line   => "\terror_page 404 /404_page.html;"
+  after  => 'server_name _;',
+  line   => "\terror_page 404 /404_page.html;",
 }
 
 file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;'
+  after  => 'server_name _;',
   line   => "\tadd_header X-Served-By ${hostname} always;"
 }
 
