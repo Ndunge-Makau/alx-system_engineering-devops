90b90747bb5061e47528faef7580926dbdc27ee8
Ndunge-Makau <winniemakau@outloo.com>
2024-11-21 14:49:23 +0300

    Fix 2-puppet_custom_http_response_header.pp: Fix directives.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,25 +6,25 @@ package {'nginx':
 
 file {'/var/www/html/index.nginx-debian.html':
   ensure  => present,
-  content => 'Hello World!',
+  content => "Hello World!",
 }
 
 file_line {'redirect':
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;'
-  line    => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  line    => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;"
 }
 
 file_line {'404':
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;'
-  line    => '\terror_page 404 /404_page.html;'
+  line    => "\terror_page 404 /404_page.html;"
 }
 
 file_line {'add header':
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;'
-  line    => '\tadd_header X-Served-By ${hostname} always;'
+  line    => "\tadd_header X-Served-By ${hostname} always;"
 }
 
 service {'nginx':
