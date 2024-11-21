026a55c2654651f5865b34b5c105e0bbc677300c
Ndunge-Makau <winniemakau@outloo.com>
2024-11-21 16:23:31 +0300

    Fix 2-puppet_custom_http_response_header.pp: Fix syntax.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,19 +9,12 @@ package { 'nginx' :
   ensure => installed,
 }
 
-file { '/var/www/html/index.nginx-debian.html' :
-  ensure  => present,
-  content => 'Hello World!',
-}
-
 file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
+  after  => 'server_name _;',
+  line   => "\tadd_header X-Served-By ${hostname} always;"
 }
-
 
 service { 'nginx' :
   ensure => running,
