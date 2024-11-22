345c39798f5668b6696fb85dbff8c85d6914c065
Ndunge-Makau <winniemakau@outloo.com>
2024-11-22 13:08:13 +0300

    Try fix.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,25 +9,6 @@ exec { 'update':
   ensure => installed,
 }
 
--> file { '/var/www/html/index.nginx-debian.html' :
-  ensure  => present,
-  content => 'Hello World!',
-}
-
-f-> ile_line { 'redirect' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;",
-}
-
--> file_line { '404' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => "\terror_page 404 /404_page.html;",
-}
-
 -> file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
