0c901550a0e4ebe12ef3ce375f2520e3ca786cbf
Ndunge-Makau <winniemakau@outloo.com>
2024-11-22 13:06:59 +0300

    Try fix.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,41 @@
 # Installs Nginx and connfigures server
 
 exec { 'update':
-  command  => 'sudo apt-get update',
+  command  => 'sudo apt-get update && sudo apt-get upgrade',
   provider => shell,
 }
 
-package { 'nginx' :
+-> package { 'nginx' :
   ensure => installed,
 }
 
-file_line { 'add header' :
+-> file { '/var/www/html/index.nginx-debian.html' :
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+f-> ile_line { 'redirect' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;",
+}
+
+-> file_line { '404' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\terror_page 404 /404_page.html;",
+}
+
+-> file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => '\tserver_name _;',
   line   => "\tadd_header X-Served-By ${hostname} always;"
 }
 
-service { 'nginx' :
+-> service { 'nginx' :
   ensure => running,
   enable => true,
 }
