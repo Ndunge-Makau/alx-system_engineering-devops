3444554bc2880ea8540a60936ab15878d3a34eca
Ndunge-Makau <winniemakau@outloo.com>
2024-11-22 12:52:42 +0300

    Try fix.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,20 @@
-# Installs Nginx and connfigures server
-
+#puppet advance
 exec { 'update':
-  command  => 'sudo apt-get update && sudo apt-get upgrade',
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
 
-package { 'nginx' :
-  ensure => installed,
-}
-
-file_line { 'add header' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => '\tserver_name _;',
-  line   => "\tadd_header X-Served-By ${hostname} always;",
-}
-
-service { 'nginx' :
-  ensure => running,
-  enable => true,
-}
